bb6f55382ce53951705a120c80156546224febd8
masiprand151 <masiprand151@gmail.com>
2026-08-25 01:27:48 +0700

    setup pos

---
+++ pos/main.js
@@ -0,0 +1,42 @@
+const { BrowserWindow, app } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    frame: false,
+    x: 0,
+    y: 0,
+    // fullscreen: true,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  if (!app.isPackaged) {
+    // DEVELOPMENT
+    win.loadURL("http://localhost:5173"); // Vite default port
+  } else {
+    // PRODUCTION / PORTABLE
+    win.loadFile(path.join(__dirname, "../dist/index.html"));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
